d1fbea67ee302bfbc2fc36bcb773f18c94938c99
shlokarth911 <shlokarth011@gmail.com>
2025-07-30 02:51:48 +0530

    refactor: Remove unused state and variable from CaptainProtectedWrapper and UserLogin components

--- frontend/src/pages/CaptainProtectedWrapper.jsx
+++ frontend/src/pages/CaptainProtectedWrapper.jsx
@@ -7,7 +7,7 @@ const CaptainProtectedWrapper = ({ children }) => {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
 
-  const { captain, setCaptain } = useContext(CaptainDataContext);
+  const { setCaptain } = useContext(CaptainDataContext);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -7,9 +7,8 @@ import axios from "axios";
 const UserLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [userData, setUserData] = useState({});
 
-  const { user, setUser } = useContext(UserDataContext);
+  const { setUser } = useContext(UserDataContext);
   const navigate = useNavigate();
 
   const SubmitHandler = async (e) => {
